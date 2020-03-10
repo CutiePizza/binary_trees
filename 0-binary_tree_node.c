1d6b8a097a7f5e342206e7a8d864d9388aeb545c
CutiePizza <ines.chokri97@gmail.com>
2020-03-10 22:41:41 +0100

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,12 +4,13 @@
  * binary_tree_node - Create a binary tree
  * @parent: the parent node of the node to create
  * @value: the value of the node
+ * Return: new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tree;
-	
+
 	tree = malloc(sizeof(binary_tree_t));
 	if (tree == NULL || parent == NULL)
 		return (NULL);
@@ -17,5 +18,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	tree->parent = parent;
 	tree->left = NULL;
 	tree->right = NULL;
-	return(tree);
+	return (tree);
 }
