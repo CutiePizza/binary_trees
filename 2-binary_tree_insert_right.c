8b9b8a1c6aa93587aeb2c30bebdc8c6520a8b4f0
CutiePizza <ines.chokri97@gmail.com>
2020-03-11 01:56:57 +0100

    task 3 done

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Insert node at riight
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in right child
+ * Return: pointer to the new node, NULL if it fails
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmp;
+
+	if (parent == NULL)
+		return (NULL);
+
+	if (parent->right == NULL)
+	{
+		parent->right = binary_tree_node(parent->right, value);
+	}
+	else
+	{
+		tmp = parent->right;
+		parent->right = binary_tree_node(parent, value);
+		parent->right->right = binary_tree_node(parent->right, tmp->n);
+	}
+	return (parent->right);
+
+}
