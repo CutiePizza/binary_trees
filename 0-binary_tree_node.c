b13e031a4a33969c7c8bdf7536fbf33fd782f3b1
ggirlk <ggirlk2@gmail.com>
2020-03-13 22:19:23 +0000

    README FILE

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,6 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	tree = malloc(sizeof(binary_tree_t));
 	if (tree == NULL)
 		return (NULL);
+
 	tree->n = value;
 	tree->parent = parent;
 	tree->left = NULL;
