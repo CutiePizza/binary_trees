518f897e77c2dc8be1c2932e8da5a0163c3014e0
CutiePizza <ines.chokri97@gmail.com>
2020-03-13 16:19:33 +0100

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,6 @@ size_t height(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-
 	return (1 + MAX(height(tree->left), height(tree->right)));
 }
 
@@ -26,6 +25,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t n;
 
+	if (tree == NULL)
+		return (0);
 	n = height(tree);
 	return (n - 1);
 }
