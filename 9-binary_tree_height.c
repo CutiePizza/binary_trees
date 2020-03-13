ab230de3a52d6e30bae95e969d32e112afe75281
CutiePizza <ines.chokri97@gmail.com>
2020-03-13 16:15:36 +0100

    task 9 done

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+#define MAX(a, b)  (a < b ? b : a)
+
+/**
+ * height - measures the height of a binary tree
+ * @tree: root node
+ * Return: size of binary tree
+ */
+
+size_t height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + MAX(height(tree->left), height(tree->right)));
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: root node
+ * Return: size of binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t n;
+
+	n = height(tree);
+	return (n - 1);
+}
